781784da666eb6092b5ce4b5f36da3a2b8a5c363
douglas henrique <33969220+douglashsoares@users.noreply.github.com>
2024-07-23 11:32:51 -0300

    Initial commit

---
+++ src/main/java/me/dio/santander_dev_week_java/SantanderDevWeekJavaApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_week_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeekJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeekJavaApplication.class, args);
+	}
+
+}
