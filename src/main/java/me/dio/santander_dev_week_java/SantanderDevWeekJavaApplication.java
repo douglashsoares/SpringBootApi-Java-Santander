0c8493e48e10d72e66b031c7f0e1a2c7e6e68ee1
douglas henrique <33969220+douglashsoares@users.noreply.github.com>
2024-08-25 13:30:47 -0300

    "BugFix CORS"

--- src/main/java/me/dio/santander_dev_week_java/SantanderDevWeekJavaApplication.java
+++ src/main/java/me/dio/santander_dev_week_java/SantanderDevWeekJavaApplication.java
@@ -1,8 +1,10 @@
 package me.dio.santander_dev_week_java;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = {@Server (url = "/", description = "Default server URL")})
 @SpringBootApplication
 public class SantanderDevWeekJavaApplication {
 
